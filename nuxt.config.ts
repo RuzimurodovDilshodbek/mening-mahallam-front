64a3aee3ec030b10ed0f3594e8914764633d3972
malikafrontend2023 <malikanasimova03@gmail.com>
2026-02-15 07:10:03 -0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://admin.mening-mahallam.uz',
     },
   },
   app: {
